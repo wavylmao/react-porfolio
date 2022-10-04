6f2776e91cff8e0fe2207d8fe7855c17e80e61a0
ddev187 <yawnhenley@gmail.com>
2022-10-04 19:54:12 -0400

    Finish mobile design

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,15 @@ import {
 } from "react-icons/ai";
 import Image from "next/image";
 import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 
 export default function Home() {
   return (
@@ -56,7 +65,7 @@ export default function Home() {
 
         <section>
           <div>
-            <h3 className="text-3xl py-1">Services I offer</h3>
+            <h3 className="text-3xl py-1 pt-10">Services I offer</h3>
             <p className="py-2 leading-8 text-gray-800">
               Since the beginning of my journey as a developer, I've done remote
               work for (I actually haven't)
@@ -65,10 +74,73 @@ export default function Home() {
               and collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            <p className="text-md py-2 leading-8 text-gray-800">
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
+          </div>
+          <div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={design} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suiting for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={code} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suiting for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={consulting} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suiting for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="py-2 leading-8 text-gray-800">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus et sunt incidunt similique eum aliquid odit rem voluptates, eligendi delectus maxime laborum, dolorum omnis. Itaque repudiandae reprehenderit non. Amet, sapiente.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea provident possimus, veniam id aperiam ipsa?
+            </p>
+          </div>
+          <div>
+            <div><Image src={web1} /></div>
+            <div><Image src={web2} /></div>
+            <div><Image src={web3} /></div>
+            <div><Image src={web4} /></div>
+            <div><Image src={web5} /></div>
+            <div><Image src={web6} /></div>
           </div>
         </section>
       </main>
